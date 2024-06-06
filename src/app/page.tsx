ece7272eb3b05c4275c08a327d74480cce5d0b45
Fxy <145225725+Fxy6969@users.noreply.github.com>
2024-06-06 12:57:55 +0000

    responsiveness

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,12 +5,12 @@ import Particles from "@/components/magicui/particles";
 // sections
 import { Footer } from "@/components/magicui/footer";
 import { Page } from "../components/home";
-
+import Header  from "@/components/custom-comps/header"
 import "./style.css";
 
 export default function Home() {
   return (
-    <>
+    <><Header />
       <Page />
       <Particles
         className="absolute inset-0 -z-10"
